2cd31a4a5c7ff5171d8250b46c45b0504f220baf
Oh Morita <65946160+mrtc-jp@users.noreply.github.com>
2020-06-08 16:11:47 +0900

    1.0.3
    シェアカウンターを削除

--- content-share.php
+++ content-share.php
@@ -9,31 +9,22 @@
 	
 	<!--Facebookボタン--> 
 	<li class="facebook">
-	  <a href="//www.facebook.com/sharer.php?src=bm&u=<?=$share_url?>&t=<?=$share_title?>" title="Facebookでシェア" onclick="javascript:window.open(this.href, '_blank', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=800,width=600');return false;">
-	    <span><?php if(function_exists('scc_get_share_facebook')) echo scc_get_share_facebook(); ?></span>
-	  </a>
+	  <a href="//www.facebook.com/sharer.php?src=bm&u=<?=$share_url?>&t=<?=$share_title?>" title="Facebookでシェア" onclick="javascript:window.open(this.href, '_blank', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=800,width=600');return false;"></a>
 	</li>
 	
 	<!-- Twitter -->
 	<li class="twitter">
-	  <a href="//twitter.com/share?text=<?=$share_title?>&url=<?=$share_url?>&via=wemo_blog" title="Twitterでシェア" onclick="javascript:window.open(this.href, '_blank', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=400,width=600');return false;">
-	    <span><?php if(function_exists('scc_get_share_twitter')) echo scc_get_share_twitter(); ?></span>
-	  </a>
+	  <a href="//twitter.com/share?text=<?=$share_title?>&url=<?=$share_url?>&via=wemo_blog" title="Twitterでシェア" onclick="javascript:window.open(this.href, '_blank', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=400,width=600');return false;"></a>
 	</li>
-	
 	
 	<!-- はてな -->
 	<li class="hatena">
-	  <a href="//b.hatena.ne.jp/add?mode=confirm&url=<?=$share_url?>" onclick="javascript:window.open(this.href, '_blank', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=1000');return false;" title="はてなブックマークに登録">
-	    <span><?php if(function_exists('scc_get_share_hatebu')) echo scc_get_share_hatebu(); ?></span>
-	  </a>
+	  <a href="//b.hatena.ne.jp/add?mode=confirm&url=<?=$share_url?>" onclick="javascript:window.open(this.href, '_blank', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=1000');return false;" title="はてなブックマークに登録"></a>
 	</li>
 	
 	<!-- ポケット -->
 	<li class="pocket">
-	  <a href="//getpocket.com/edit?url=<?=$share_url?>&title=<?=$share_title?>" target="_blank" title="Pocketに保存する">
-	    <span><?php if(function_exists('scc_get_share_pocket')) echo scc_get_share_pocket(); ?></span>
-	  </a>
+	  <a href="//getpocket.com/edit?url=<?=$share_url?>&title=<?=$share_title?>" target="_blank" title="Pocketに保存する"></a>
 	</li>
 	
 	</ul>
