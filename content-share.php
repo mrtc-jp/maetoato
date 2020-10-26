28daf593678050be904f38c810f86f2e16ba3c55
Oh Morita <65946160+mrtc-jp@users.noreply.github.com>
2020-10-26 11:06:14 +0900

    1.1.2

--- content-share.php
+++ content-share.php
@@ -3,30 +3,23 @@
   $share_title = get_the_title();
 ?>
 <div class="share">
-
 	<h2 class="">Share Post</h2>
 	<ul class="sns-share menu">
-	
-	<!--Facebookボタン--> 
-	<li class="facebook">
-	  <a href="//www.facebook.com/sharer.php?src=bm&u=<?=$share_url?>&t=<?=$share_title?>" title="Facebookでシェア" onclick="javascript:window.open(this.href, '_blank', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=800,width=600');return false;"></a>
-	</li>
-	
-	<!-- Twitter -->
-	<li class="twitter">
-	  <a href="//twitter.com/share?text=<?=$share_title?>&url=<?=$share_url?>&via=wemo_blog" title="Twitterでシェア" onclick="javascript:window.open(this.href, '_blank', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=400,width=600');return false;"></a>
-	</li>
-	
-	<!-- はてな -->
-	<li class="hatena">
-	  <a href="//b.hatena.ne.jp/add?mode=confirm&url=<?=$share_url?>" onclick="javascript:window.open(this.href, '_blank', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=1000');return false;" title="はてなブックマークに登録"></a>
-	</li>
-	
-	<!-- ポケット -->
-	<li class="pocket">
-	  <a href="//getpocket.com/edit?url=<?=$share_url?>&title=<?=$share_title?>" target="_blank" title="Pocketに保存する"></a>
-	</li>
-	
+		
+		<!--Shareボタン1--> 
+		<li class="Facebook">
+			<div class="shareaholic-canvas" data-app="share_buttons" data-app-id="28985675"></div>
+		</li>
+		
+		<!--Twitter-->
+		<li class="Twitter">
+			<a href="http://twitcount.com/btn" class="twitcount-button" data-count="vertical" data-size="" data-url="" data-text="" data-related="" data-hashtag="" data-via="maetoato_web">TwitCount Button</a><script type="text/javascript" src="https://static1.twitcount.com/js/button.js"></script>
+		</li>
+		
+		<!-- はてな -->
+		<li class="hatena">
+			<a href="//b.hatena.ne.jp/add?mode=confirm&url=<?=$share_url?>" onclick="javascript:window.open(this.href, '_blank', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=1000');return false;" title="はてなブックマークに登録"></a>
+		</li>
 	</ul>
-
 </div>
+	
